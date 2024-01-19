d91a647a180a03c7b3563efa199026bf804881c3
thecodercoder <37986728+thecodercoder@users.noreply.github.com>
2024-01-19 16:45:03 -0600

    Finished

---
+++ script.js
@@ -0,0 +1,46 @@
+const btnOpen = document.querySelector('#btnOpen');
+const btnClose = document.querySelector('#btnClose');
+const media = window.matchMedia('(width < 40em)');
+const topNavMenu = document.querySelector('.topnav__menu');
+const main = document.querySelector('main');
+
+function openMobileMenu() {
+  btnOpen.setAttribute('aria-expanded', 'true');
+  topNavMenu.removeAttribute('inert');
+  topNavMenu.removeAttribute('style');
+  main.setAttribute('inert', '');
+  btnClose.focus();
+}
+
+function closeMobileMenu() {
+  btnOpen.setAttribute('aria-expanded', 'false');
+  topNavMenu.setAttribute('inert', '');
+  main.removeAttribute('inert');
+  btnOpen.focus();
+
+  setTimeout(() => {
+    topNavMenu.style.transition = 'none';
+  }, 500);
+}
+
+function setupTopNav(e) {
+  if (e.matches) {
+    // is mobile
+    console.log('is mobile');
+    topNavMenu.setAttribute('inert', '');
+    topNavMenu.style.transition = 'none';
+  } else {
+    // is tablet/desktop
+    console.log('is desktop');
+    topNavMenu.removeAttribute('inert');
+  }
+}
+
+setupTopNav(media);
+
+btnOpen.addEventListener('click', openMobileMenu);
+btnClose.addEventListener('click', closeMobileMenu);
+
+media.addEventListener('change', function (e) {
+  setupTopNav(e);
+});
